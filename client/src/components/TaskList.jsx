d17cc58612f45cc8854dbb4a158cc20349de7343
Denny Petersson <petersson.denny@gmail.com>
2024-11-26 17:13:50 +0100

    creating task list with task items

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,6 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import { fetchTasks } from '../utils/api'
+// import { fetchTasks } from '../utils/api'
 import TaskListItem from './TaskListItem'
+
+const tasksDummy = [
+  {
+    task_id: 1,
+    title: 'Task 1',
+    description: 'Description 1',
+    status: 'Status 1',
+    due_date: '2023-01-01',
+  },
+  {
+    task_id: 2,
+    title: 'Task 2',
+    description: 'Description 2',
+    status: 'Status 2',
+    due_date: '2023-01-02',
+  },
+  {
+    task_id: 3,
+    title: 'Task 3',
+    description: 'Description 3',
+    status: 'Status 3',
+    due_date: '2023-01-03',
+  },
+  {
+    task_id: 4,
+    title: 'Task 4',
+    description: 'Description 4',
+    status: 'Status 4',
+    due_date: '2023-01-04',
+  },
+  {
+    task_id: 5,
+    title: 'Task 5',
+    description: 'Description 5',
+    status: 'Status 5',
+    due_date: '2023-01-05',
+  },
+]
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([])
@@ -8,33 +46,21 @@ const TaskList = () => {
   useEffect(() => {
     const getTasks = async () => {
       try {
-        const data = await fetchTasks()
-        console.log('🚀 ~ getTasks ~ data:', data)
+        const data = tasksDummy
         setTasks(data)
-      } catch (error) {}
+      } catch (error) {
+        console.error('error fetching tasks', error)
+      }
     }
 
     getTasks()
   }, [])
 
-  const extractDate = (datetime) => {
-    return datetime.split('T')[0]
-  }
-
   return (
     <>
-      <ul className="divide-y-2">
+      <ul className="divide-y-1">
         {tasks.map((task, index) => (
-          <li
-            key={task.task_id}
-            className="bg-primary text-white rounded-lg h-24"
-          >
-            <div className="mx-2 pb-2 h-full">
-              <p className="text-lg font-semibold leading-6">{task.title}</p>
-              <p className="text-sm">{task.description}</p>
-              <p className="text-sm">Due on: {extractDate(task.due_date)}</p>
-            </div>
-          </li>
+          <TaskListItem task={task} key={task.task_id} />
         ))}
       </ul>
     </>
--- client/src/components/TaskListItem.jsx
+++ client/src/components/TaskListItem.jsx
@@ -1,7 +1,33 @@
+import { ArrowRightCircleIcon } from '@heroicons/react/16/solid'
 import React from 'react'
 
-const TaskListItem = () => {
-  return <div>TaskListItem</div>
+const extractDate = (datetime) => {
+  return datetime.split('T')[0]
+}
+
+const TaskListItem = ({ task, taskid }) => {
+  return (
+    <li
+      key={taskid}
+      className="my-2 bg-primary text-black rounded-sm grid grid-cols-12"
+    >
+      <div className="mx-2 h-full col-span-10 grid grid-rows-4">
+        <h3 className="font-semibold leading-6 row-span-1 pt-1">
+          {task.title}
+        </h3>
+        <p className="text-sm row-span-1">{task.description}</p>
+        <p className="text-sm row-span-1">{task.status}</p>
+        <p className="text-sm row-span-1 pb-1">
+          Due on: {extractDate(task.due_date)}
+        </p>
+      </div>
+      <div className="col-span-2 content-center justify-self-center">
+        <a href="#">
+          <ArrowRightCircleIcon className="text-secondary size-8" />
+        </a>
+      </div>
+    </li>
+  )
 }
 
 export default TaskListItem
