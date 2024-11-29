ae51b6b4dac683243e76cad21194376b68d29c83
Denny Petersson <petersson.denny@gmail.com>
2024-11-29 20:00:17 +0100

    adding navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import {
+  BellIcon,
+  CalendarDaysIcon,
+  ChatBubbleLeftRightIcon,
+  HomeIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/24/outline'
+
+import React from 'react'
+
+const NavBar = ({ handleAddTask }) => {
+  return (
+    <nav className="grid grid-cols-5 justify-items-center fixed bottom-0 w-full bg-secondary py-4">
+      <a href="#">
+        <HomeIcon className="size-8 text-primary" />
+      </a>
+      <a href="#">
+        <ChatBubbleLeftRightIcon className="size-8 text-primary" />
+      </a>
+      <a href="#">
+        <PlusCircleIcon
+          className="size-8 text-primary"
+          onClick={handleAddTask}
+        />
+      </a>
+      <a href="#">
+        <CalendarDaysIcon className="size-8 text-primary" />
+      </a>
+      <a href="#">
+        <BellIcon className="size-8 text-primary" />
+      </a>
+    </nav>
+  )
+}
+
+export default NavBar
